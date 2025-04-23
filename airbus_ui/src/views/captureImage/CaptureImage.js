2cddf08e45b8ec87a96386ad18def85d33b791bf
Nour <achahlaou.nour@gmail.com>
2025-04-23 18:45:14 +0100

    feat: init user management microservice with JWT auth, email config, and user model

--- airbus_ui/src/views/captureImage/CaptureImage.js
+++ airbus_ui/src/views/captureImage/CaptureImage.js
@@ -1,8 +1,26 @@
 // pages/Home.js
 import React from 'react';
-
+import { Box, Grid, Card } from '@mui/material';
 const CaptureImage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' } }}>
+      <Grid
+        container
+        spacing={2}
+        columns={12}
+        sx={{ mb: (theme) => theme.spacing(2) }}
+      >
+        <Grid size={{ xs: 12, md: 9 }}>
+            <Card variant="outlined" sx={{ width: '100%' }}>
+            </Card>
+        </Grid>
+        <Grid size={{ xs: 12, md: 3 }}>
+            <Card variant="outlined" sx={{ width: '100%' }}>
+            </Card>
+        </Grid>
+    </Grid>
+    </Box>
+  );
 };
 
 export default CaptureImage;
